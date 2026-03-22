a0efc5a8f5090308073707cdf18e0cc19dfae70f
Adriik <adriik.linux@proton.me>
2026-03-22 08:09:23 -0300

    chore: initial commit
    Signed-off-by: Adriik <adriik.linux@proton.me>

---
+++ src/main/java/prog3/tp/Main.java
@@ -0,0 +1,9 @@
+package prog3.tp;
+
+public class Main
+{
+        public static void main(String[] args)
+        {
+                System.out.println("Hello World!");
+        }
+}
---
+++ src/test/java/prog3/tp/MainTest.java
@@ -0,0 +1,13 @@
+package prog3.tp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest
+{
+        @Test
+        public void testSomething()
+        {
+                assertEquals(4, 2 + 2);
+        }
+}
