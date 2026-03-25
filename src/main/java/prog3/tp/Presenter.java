47f62f7dccacebbce71cabc58acd6a93b056f427
Adriik <adriik.linux@proton.me>
2026-03-25 11:24:24 -0300

    feat: add Presenter constructor
    Signed-off-by: Adriik <adriik.linux@proton.me>

--- src/main/java/prog3/tp/Presenter.java
+++ src/main/java/prog3/tp/Presenter.java
@@ -2,6 +2,18 @@ package prog3.tp;
 
 class Presenter implements Observer
 {
+    private final Model _model;
+    private final View _view;
+
+    public Presenter(Model model, View view)
+    {
+        _model = model;
+        _view = view;
+
+        _view.setPresenter(this);
+        _model.addObserver(this);
+    }
+
     @Override
     public void update()
     {}
